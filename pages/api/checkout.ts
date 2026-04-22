28f6b368ace756900b01177431af77c4c8c306f5
adpulsea <adpulsea@gmail.com>
2026-04-22 18:41:42 +0100

    Update checkout.ts

--- pages/api/checkout.ts
+++ pages/api/checkout.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2026-03-25.dahlia",
 })
 
 export default async function handler(req, res) {
